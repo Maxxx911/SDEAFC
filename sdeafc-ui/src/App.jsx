b0a831954566d16d6dd6e473d866ea9f51a0860c
Maxim Spasyonov <m.spasyonov@invento.by>
2023-07-31 09:38:07 +0300

    Added draw io container

--- sdeafc-ui/src/App.jsx
+++ sdeafc-ui/src/App.jsx
@@ -9,6 +9,7 @@ import { BrowserRouter } from "react-router-dom";
 import Editor from './components/editor/Editor';
 import FormBuilder from './components/formBuilder/FormBuilder';
 import DocumentLayout from './components/documentLayout/DocumentLayout';
+import DrawIOContainer from './components/drawIO/DrawIO';
 
 const App = () => {
   return (
@@ -18,11 +19,13 @@ const App = () => {
       <Link className='button' to="/">Editor</Link>
       <Link className='button' to="/form-builder">Builder Form</Link>
       <Link className='button' to="/document-layout">Document Layout</Link>
+      <Link className="button" to="/draw-io-document-layout">Draw IO</Link>
      </div>
       <Routes>
         <Route path='/' element={<Editor />}/>
         <Route path='/form-builder' element={<FormBuilder />} />
         <Route path='/document-layout' element={<DocumentLayout />}/>
+        <Route path='/draw-io-document-layout' element={<DrawIOContainer/>}/>
        </Routes>
       </BrowserRouter>
     </div>
---
+++ sdeafc-ui/src/components/drawIO/DrawIO.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const DrawIOContainer = () => {
+
+  return (
+    <iframe></iframe>
+  )
+};
+
+export default DrawIOContainer;
+
+
+
